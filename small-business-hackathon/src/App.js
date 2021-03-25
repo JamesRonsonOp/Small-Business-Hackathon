a6334b88c5df27733fcc2f149df2b4bb68576fec
Thea Harris <supershay@Theas-MacBook-Pro.local>
2021-03-24 20:04:02 -0400

    Route paths added to App.js

--- small-business-hackathon/src/App.js
+++ small-business-hackathon/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from './Components/Dashboard/dashboard';
+import Menu from './Components/Menu/menu';
+import Reservations from './Components/Reservations/reservations';
 
 function App() {
   return (
     
-    <p>Small Business</p>
+    <Router>
+    <Switch>
+    <Route path={"/dashboard"} component={Dashboard}/>
+    <Route path={"/menu"} component={Menu}/>
+    <Route path={"/reservations"} component={Reservations}/>
+    </Switch>
+    </Router>
 
   );
 }
