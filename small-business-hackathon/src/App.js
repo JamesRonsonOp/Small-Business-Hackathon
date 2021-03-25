3b29e64b3c8239952024d114ec2baac13da23961
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-25 00:07:35 -0500

    Renamed Navigate to Navbar

--- small-business-hackathon/src/App.js
+++ small-business-hackathon/src/App.js
@@ -5,20 +5,20 @@ import Dashboard from './Components/Dashboard';
 import Menu from './Components/Menu';
 import Reservations from './Components/Reservations';
 import Account from './Components/Account';
-import Navigate from './Components/Navbar';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <Navigate />
+    <Navbar />
         <Switch>
             <Route path='/' exact component={Dashboard} />
-            <Route path='/menu' exact component={Menu} />
-            <Route path='/reservations' exact component={Reservations} />
-            <Route path='/account' exact component={Account} />
+            <Route path='/menu' component={Menu} />
+            <Route path='/reservations' component={Reservations} />
+            <Route path='/account' component={Account} />
         </Switch>
     </div>
-  );
-}
+  )
+};
 
 export default App;
